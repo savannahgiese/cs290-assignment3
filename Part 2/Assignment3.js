a51d252e77e563c36e469cd126324d0cb8ea63e5
Savannah Giese <gieses@onid.oregonstate.edu>
2015-02-01 14:50:13 -0800

    Finished assignment 3

--- Part 2/Assignment3.js
+++ Part 2/Assignment3.js
@@ -1,5 +1,6 @@
 var requests = [];
-var favorites = [];
+var favorites = {};
+var results = {};
 var checkedLanguages = [];
 var table;
 var tableFavorites;
@@ -7,17 +8,22 @@ var tableFavorites;
 function startUp() {
     table = document.getElementById("results");
     tableFavorites = document.getElementById("favorites");
+    favorites = JSON.parse(localStorage.getItem("favorites"));
+    if(!favorites) {
+        favorites = {};
+    }
+    else {
+        generate_table("favorites", favorites);
+    }
 }
 
 function HulkSmash() {
-    while(table.rows.length > 0) { 
-        table.deleteRow(0); 
-    }
-    //console.log("Hulk Smash!");
+    
 }
 
 function searchOptions() {
     checkedLanguages = [];
+    results = {};
     var tempLang = document.getElementsByClassName("language");
     var e = document.getElementById("pageCount");
     var pageCount = e.options[e.selectedIndex].value;
@@ -29,7 +35,6 @@ function searchOptions() {
     }
 
     var thisRequest;
-    HulkSmash();
     for (var i = 0; i < pageCount; i++) {
         thisRequest = new XMLHttpRequest();
         requests.push({
@@ -41,8 +46,8 @@ function searchOptions() {
             return false;
         }
         thisRequest.onreadystatechange = processAjax;
-        thisRequest.open('GET', 'http://savvyg.me/CS_290/cs290-assignment3/Part%202/gists' + (i + 1) + '.json');
-        //thisRequest.open('GET', 'https://api.github.com/gists?page=' + i);
+        //thisRequest.open('GET', 'http://savvyg.me/CS_290/cs290-assignment3/Part%202/gists' + (i + 1) + '.json');
+        thisRequest.open('GET', 'https://api.github.com/gists?page=' + (i + 1));
         thisRequest.send();
     }
 
@@ -58,7 +63,7 @@ function processAjax() {
                 if (requests[k].req.status === 200) {
                     //console.log("it worked");
                     gists = JSON.parse(requests[k].req.response);
-                    addRows(filterArray(gists));
+                    addRows(filterArray(gists), "results", results);
                     requests[k].done = true;
                 }
                 else {
@@ -78,25 +83,71 @@ function filterArray(gists) {
         l = files[Object.keys(files)[0]].language;
         //console.log(l);
         if (checkedLanguages.indexOf(l) > -1 || checkedLanguages.length == 0) {
-            console.log(l);
-            if (favorites.indexOf(gists[k].url) < 0) {
-                newGists.push(gists[k]);
+            //console.log(l);
+            if (!favorites.hasOwnProperty(gists[k]['id'])) {
+                newGists[gists[k]['id']] = {
+                    description: gists[k].description,
+                    url: gists[k].html_url
+                };
             }
         }
     }
     return newGists;
 }
 
-function addRows(gists) {
-    console.log(gists);
+function addRows(inArray, tableId, outArray) {
+    //console.log(gists);
     var row;
-    
-    for (var i = 0; i < gists.length; i++) {
-        row = table.insertRow();
-        row.innerHTML = "<td><a href=" + gists[i].html_url + ">" + gists[i].id + " - " + gists[i].description + "</a></td><td><button onclick=favorite('" + gists[i].id + "')>Favorite</button></td>";
+    var id;
+    for (id in inArray) {
+        outArray[id] = inArray[id];
     }
+    generate_table(tableId, outArray);
+
 }
 
 function favorite(gistId) {
-    
+    favorites[gistId] = results[gistId];
+    delete results[gistId];
+    generate_table("results", results);
+    console.log(favorites);
+    generate_table("favorites", favorites);
+    saveFavorites();
+}
+
+function generate_table(tableId, data) {
+
+    var tblBody = document.getElementById(tableId);
+    //var tblBody = document.createElement("tbody");
+
+    tblBody.innerHTML = "";
+    console.log(data);
+    for (var id in data) {
+        var row = tblBody.insertRow();
+        var cell1 = document.createElement("td");
+        var cell2 = document.createElement("td");
+        cell1.innerHTML = ("<a href=" + data[id].url + ">" + id + " - " + data[id].description + "</a>");
+        if (tableId == "results") {
+            cell2.innerHTML = ("<button onclick=favorite('" + id + "')>Favorite</button>");
+        }
+        else if (tableId == "favorites") {
+            cell2.innerHTML = ("<button onclick=deleteFavorite('" + id + "')>Remove</button>");
+        }
+        row.appendChild(cell1);
+        row.appendChild(cell2);
+    }
+    if (row)
+    {
+        tblBody.appendChild(row);
+    }
+}
+
+function deleteFavorite(gistId) {
+    delete favorites[gistId];
+    saveFavorites();
+    generate_table("favorites", favorites);
+}
+
+function saveFavorites() {
+    localStorage.setItem("favorites", JSON.stringify(favorites))
 }
